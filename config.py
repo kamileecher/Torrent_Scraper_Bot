9c5d33fd8f79063902648a525c77f3379e9e31a7
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-05-16 11:10:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ api_id = 5412484
 api_hash = "37402de39e5eb30d378bf20823db1dfc"
 bot_token = "1816330290:AAGaZiTke_SSsM350GLi5j805II06gIyWFc"
 ARQ_API_KEY = "LGBORE-ILYVPL-WSKKFQ-PEIYQY-ARQ"
-ARQ_API_BASE_URL = "https://thearq.tech"
+ARQ_API_BASE_URL = "https://thearq.tech/"
