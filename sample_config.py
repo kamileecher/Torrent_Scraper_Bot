cf41f8de80837aa8db2d94b58ec228b6060611aa
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-05-16 10:19:23 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,5 +1,5 @@
-api_id = 69420
-api_hash = "854qa6e5rfawse6f7a6w8e7a74f63sd57f6we"
-bot_token = "3as4f65saf:fa5s7fa5w7f5877sf5aw6e8r7fa6"
-ARQ_API_KEY = "Get this from @ARQRobot"
+api_id = 5412484
+api_hash = "37402de39e5eb30d378bf20823db1dfc"
+bot_token = "1816330290:AAGaZiTke_SSsM350GLi5j805II06gIyWFc"
+ARQ_API_KEY = "LGBORE-ILYVPL-WSKKFQ-PEIYQY-ARQ"
 ARQ_API_BASE_URL = "https://thearq.tech"
